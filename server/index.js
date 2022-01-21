149889ec4b4eb0629460819bdd993d8b7b266d72
Gouthum Karadi <karadi@virtualnex.us>
2022-01-21 03:12:48 -0400

    Successfully implemented secp256k1 curve, utils - Uint32Array.toHex, pubkey from private.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 const { sha256 } = require("ethereum-cryptography/sha256");
-const { utf8ToBytes } = require("ethereum-cryptography/utils");
-const { createPrivateKeySync, ecdsaSign } = require("ethereum-cryptography/secp256k1")
+const secp = require("ethereum-cryptography/secp256k1");
+const { hexToBytes, concatBytes, toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
 
 // localhost can have cross origin errors
@@ -12,17 +12,40 @@ const { createPrivateKeySync, ecdsaSign } = require("ethereum-cryptography/secp2
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "0x44Ae1DA1b5D6E15f0d8e039207fe503Ab33BA536": 100,
-  "0x775d0133FA6eE0B5Ca5eEFf6fD5667Ca57650Ee6": 50,
-  "0x032E98D0b16eF88fC467009FBAfD8078dBD1A1c1": 75,
-}
+const keyArray1 = secp.utils.randomPrivateKey();
+const keyArray2 = secp.utils.randomPrivateKey();
+const keyArray3 = secp.utils.randomPrivateKey();
+
+const pubkeyArray1 = secp.getPublicKey(keyArray1);
+const pubkeyArray2 = secp.getPublicKey(keyArray2);
+const pubkeyArray3 = secp.getPublicKey(keyArray3);
+
+const key1 = toHex(keyArray1);
+const key2 = toHex(keyArray2);
+const key3 = toHex(keyArray3);
+
+const pubkey1 = toHex(pubkeyArray1);
+const pubkey2 = toHex(pubkeyArray2);
+const pubkey3 = toHex(pubkeyArray3);
+
+
+const balances = {}
+
+balances[pubkey1] = 100;
+balances[pubkey2] = 50;
+balances[pubkey3] = 75;
+
+console.log(balances);
 
 const keys = {
-  key1: "0xb8e07b69eceeef6e2ba731c02b65945449a393be673353e0dd7c543a22b6b3c8",
-  key2: "0x2ad0d612c426720e19c99c3ee691a86fb1e8cf3cc6d30ad1df48e916cbd9a417",
-  key3: "0xc0b10960727aad557a00c7804d1c73f184587574f0f51ab247bc774091bcb41f"
+  key1,
+  key2,
+  key3
 }
+
+console.log(keys);
+
+
 
 //Define functions from app.post
 //Take argument of Private Key from request body in client.index.js
@@ -30,13 +53,25 @@ const keys = {
 //Compare hash of private key matching to balance pubkey
 //If they match, subtract the value from pubkey balances and add to recipient key balances
 
-function getPublicKey(privateKey: Uint8Array, isCompressed?: false): Uint8Array;
-function getSharedSecret(privateKeyA: Uint8Array, publicKeyB: Uint8Array): Uint8Array;
-function sign(msgHash: Uint8Array, privateKey: Uint8Array, opts?: Options): Promise<Uint8Array>;
-function signSync(msgHash: Uint8Array, privateKey: Uint8Array, opts?: Options): Uint8Array;
-function verify(signature: Uint8Array, msgHash: Uint8Array, publicKey: Uint8Array): boolean
-function recoverPublicKey(msgHash: Uint8Array, signature: Uint8Array, recovery: number): Uint8Array | undefined;
-function utils.randomPrivateKey(): Uint8Array;
+//function getPublicKey(privateKey: Uint8Array, isCompressed?: false): Uint8Array{};
+
+//function getSharedSecret(privateKeyA: Uint8Array, publicKeyB: Uint8Array): Uint8Array{};
+//function sign(msgHash: Uint8Array, privateKey: Uint8Array, opts?: Options): Promise<Uint8Array>{};
+//function verify(signature: Uint8Array, msgHash: Uint8Array, publicKey: Uint8Array): boolean{};function verify(signature, msgHash, publicKey){};
+//function recoverPublicKey(msgHash: Uint8Array, signature: Uint8Array, recovery: number): Uint8Array | undefined{};
+//function utils.randomPrivateKey(): Uint8Array;
+
+
+(async () => {
+  // You pass either a hex string, or Uint8Array
+  const privateKey = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+  const messageHash = "a33321f98e4ff1c283c76998f14f57447545d339b3db534c6d886decb4209f28";
+  const publicKey = secp.getPublicKey(privateKey);
+  const signature = await secp.sign(messageHash, privateKey);
+  const isSigned = secp.verify(signature, messageHash, publicKey);
+})();
+
+
 
 function hashAmount(amount) {
   sha256(utf8ToBytes(amount));
