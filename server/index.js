de4fed2779e79c7f0bb117ae6ecd59b3b95717f0
Gouthum Karadi <karadi@virtualnex.us>
2022-01-24 21:12:13 -0400

    Everything works, however the verify seems questionable. It verifies that the pubkeyDervied from the private key has signed its key. Need to use a different pubkey from the local buffer.

--- server/index.js
+++ server/index.js
@@ -29,9 +29,6 @@ const pubkey1 = toHex(pubkeyArray1);
 const pubkey2 = toHex(pubkeyArray2);
 const pubkey3 = toHex(pubkeyArray3);
 
-//const senderPrivKey = '';
-//const senderPubkey = '';
-
 const balances = {}
 
 balances[pubkey1] = 100;
@@ -49,15 +46,7 @@ const keys = {
 console.log(keys);
 
 
-
-//Define functions from app.post
-//Take argument of Private Key from request body in client.index.js
-//Call sha256() hash request with private key (utf8ToBytes)
-//Compare hash of private key matching to balance pubkey
-//If they match, subtract the value from pubkey balances and add to recipient key balances
-
 //function getPublicKey(privateKey: Uint8Array, isCompressed?: false): Uint8Array{};
-
 //function getSharedSecret(privateKeyA: Uint8Array, publicKeyB: Uint8Array): Uint8Array{};
 //function sign(msgHash: Uint8Array, privateKey: Uint8Array, opts?: Options): Promise<Uint8Array>{};
 //function verify(signature: Uint8Array, msgHash: Uint8Array, publicKey: Uint8Array): boolean{};function verify(signature, msgHash, publicKey){};
@@ -79,18 +68,7 @@ console.log(keys);
 })();
 */
 
-/*
-Sign message upon button.onClick();
-1. scep.sign(msgHash, privateKey,)
-*/
 
-/*
-Verify operation on server end 
-1. Receive signed hash with message and publicKey.
-2. Run function scep.verify(signature, msgHash, publicKey)
-3. If 2. true, commit transaction.
-4. If false, return error to page.
-*/
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -107,14 +85,16 @@ app.post('/send', async (req, res) => {
 
   let amountMessageHash = sha256(utf8ToBytes(amount));
   let signature = await secp.sign(sha256(utf8ToBytes(amount)), sender);
-  verifiedResults = secp.verify(signingResults[0], signingResults[1], publicKeyDerived )
+  verifiedResults = secp.verify(signingResults[0], signingResults[1], publicKeyDerived);
+  console.log(verifiedResults);
+  if(verifiedResults){
   balances[senderPubkey] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
   console.log({balance: balances[senderPubkey]});
-  res.send({ balance: balances[senderPubkey] }); /*}
+  res.send({ balance: balances[senderPubkey] }); }
   else {
     res.send({ balance: "Invalid Transaction "});
-  }*/
+  }
 });
 
 app.listen(port, () => {
